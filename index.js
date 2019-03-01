8ca142b7f039c73d9a79fc53602e58ac9d9d7398
Merline Belizaire <merline@noukod.com>
2019-03-01 17:34:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // let neighborhoodId = 0;
 // let customerId = 0;
 // let mealId = 0;
 // let deliveryId = 0;
-// 
+//
 // //has many deliveries
 // //has many customers, through deliveries
 // //has many meals, through deliveries
@@ -53,29 +53,29 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 //   constructor(name) {
 //     this.name = name;
 //     this.id = ++neighborhoodId;
-// 
+//
 //     store.neighborhoods.push(this)
 //   }
-// 
+//
 //   deliveries() {
 //     return store.deliveries.filter(delivery => {
 //       return delivery.neighborhoodId === this.id
 //     })
 //   }
-// 
+//
 //   customers() {
 //     return store.customers.filter(customer => {
 //       return customer.neighborhoodId === this.id
 //     })
 //   }
-// 
+//
 //   meals() {
 //     const meals = this.deliveries().map(delivery =>
 //       delivery.meal())
 //     return [...new Set(meals)]
 //   }
 // }
-// 
+//
 // //belongs to a neighborhood
 // //has many deliveries
 // //has many meals, through deliveries
@@ -84,56 +84,56 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 //     this.name = name;
 //     this.id = ++customerId;
 //     this.neighborhoodId = neighborhoodId;
-// 
+//
 //     store.customers.push(this)
 //   }
-// 
+//
 //   deliveries() {
 //     return store.deliveries.filter(delivery => {
 //       return delivery.customerId === this.id
 //     })
 //   }
-// 
+//
 //   meals() {
 //     return this.deliveries().map(delivery => {
 //       return delivery.meal();
 //     })
 //   }
-// 
+//
 //   totalSpent() {
 //     return this.meals().reduce((total, meal) => total + meal.price, 0)
 //   }
 // }
-// 
+//
 // //has many customers
 // class Meal {
 //   constructor(title, price){
 //     this.title = title;
 //     this.price = price;
 //     this.id = ++mealId;
-// 
+//
 //     store.meals.push(this)
 //   }
-// 
+//
 //   deliveries() {
 //     return store.deliveries.filter(delivery => {
 //       return delivery.mealId === this.id;
 //     })
 //   }
-// 
+//
 //   customers() {
 //     return this.deliveries().map(delivery => {
 //       return delivery.customer();
 //     })
 //   }
-// 
+//
 //   static byPrice() {
 //     return store.meals.sort(function (a, b) {
 //       return b.price - a.price;
 //     })
 //   }
 // }
-// 
+//
 // //JOIN TABLE
 // //belongs to meal
 // //belongs to customer
@@ -144,22 +144,22 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 //     this.neighborhoodId = neighborhoodId;
 //     this.customerId = customerId;
 //     this.id = ++deliveryId;
-// 
+//
 //     store.deliveries.push(this)
 //   }
-// 
+//
 //   meal() {
 //     return store.meals.find(meal => {
 //       return meal.id === this.mealId;
 //     })
 //   }
-// 
+//
 //   customer() {
 //     return store.customers.find(customer => {
 //       return customer.id === this.customerId;
 //     })
 //   }
-// 
+//
 //   neighborhood() {
 //     return store.neighborhoods.find(neighborhood => {
 //       return neighborhood.id === this.neighborhoodId;
