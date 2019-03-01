26bb676b8ca9d5d65b235028d9f177d37b42cb51
Merline Belizaire <merline@noukod.com>
2019-03-01 17:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,60 +2,18 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let neighborhoodId = 0;
 let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;
 
-// //has many deliveries
-// //has many customers, through deliveries
-// //has many meals, through deliveries
-// class Neighborhood {
-//   constructor(name) {
-//     this.name = name;
-//     this.id = ++neighborhoodId;
-//
-//     store.neighborhoods.push(this)
-//   }
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    this.id = ++neighborhoodId;
+
+    store.neighborhoods.push(this)
+  }
 //
 //   deliveries() {
 //     return store.deliveries.filter(delivery => {
