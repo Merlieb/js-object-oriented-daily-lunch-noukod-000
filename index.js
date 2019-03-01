88780cba074ee59a587e0017bafb23088c37aef6
Merline Belizaire <merline@noukod.com>
2019-03-01 17:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ let customerId = 0;
 let mealId = 0;
 let deliveryId = 0;
 
-class Neighborhood {
-  constructor(name) {
-    this.name = name;
-    this.id = ++neighborhoodId;
-
-    store.neighborhoods.push(this)
-  }
+// class Neighborhood {
+//   constructor(name) {
+//     this.name = name;
+//     this.id = ++neighborhoodId;
+//
+//     store.neighborhoods.push(this)
+//   }
 //
 //   deliveries() {
 //     return store.deliveries.filter(delivery => {
