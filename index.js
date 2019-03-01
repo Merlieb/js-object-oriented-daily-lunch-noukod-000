e49a5af1761754634be892f728bde661c3d89780
Merline Belizaire <merline@noukod.com>
2019-03-01 17:35:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// let neighborhoodId = 0;
-// let customerId = 0;
-// let mealId = 0;
-// let deliveryId = 0;
-//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
 // //has many deliveries
 // //has many customers, through deliveries
 // //has many meals, through deliveries
